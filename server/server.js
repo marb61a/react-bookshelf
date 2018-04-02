569b0f3006f9a99dca932d6ce910b478e252da6f
martin byrne <marb61a@gmail.com>
2018-04-02 16:09:31 +0000

    added book model

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const config = require('./config/config').get(process.env.NODE_ENV);
+const app = express();
+
+const { User } = require('./models/user'); 
+const { Book } = require('./models/book');
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`SERVER RUNNNING`);
+});
