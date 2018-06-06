df92e5e0d4f67c1d72607a1d6b15f5fbf788760c
martin byrne <marb61a@gmail.com>
2018-06-06 20:00:46 +0000

    added post login function to seerver.js

--- server/server.js
+++ server/server.js
@@ -102,7 +102,9 @@ app.post('/api/book', (req, res) => {
     const book = new Book(req.body);
     
     book.save((err, doc) => {
-        if(err) return res.status(400).send(err);
+        if(err) {
+            return res.status(400).send(err);
+        }
         
         res.status(200).json({
             post: true,
@@ -122,6 +124,38 @@ app.post('/api/register', (req, res) => {
         res.status(200).json({
             success: true,
             user: doc
+        });
+    });
+});
+
+app.post('/api/login', (req, res) => {
+    User.findOne({'email': req.body.email}, (err, user) => {
+        if(!user){
+            return res.json({
+                isAuth: false,
+                message:'Auth failed, email not found'
+            });
+        }
+        
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch){
+                return res.json({
+                    isAuth:false,
+                    message:'Wrong password'
+                });
+            }
+            
+            user.generateToken((err, user) => {
+                if(err){
+                    return res.status(400).send(err);
+                }
+                
+                res.cookie('auth', user.token).json({
+                    isAuth: true,
+                    id: user._id,
+                    email: user.email
+                });
+            });
         });
     });
 });
