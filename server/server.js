32daa8434a60b95ad9e9a6108a1b10d84f214923
martin byrne <marb61a@gmail.com>
2018-06-07 10:34:13 +0000

    added gitignore and update and delete api methods to server.js

--- server/server.js
+++ server/server.js
@@ -160,10 +160,35 @@ app.post('/api/login', (req, res) => {
     });
 });
 
+
 // UPDATE routes
+app.post('/api/book_update', (req, res) => {
+    Book.findByIdAndUpdate(req.body._id, req.body, {new: true}, (err, doc) => {
+        if(err){
+            return res.status(400).send(err);
+        }        
+        
+        res.json({
+            success: true,
+            doc
+        });
+    });
+});
 
 
 // DELETE routes
+app.delete('/api/delete_book', (req, res) => {
+    let id = req.query.id;
+    
+    Book.findByIdAndRemove(id, (err, doc) => {
+        if(err){
+            return res.status(400).send(err);
+        } 
+        
+        res.json(true);
+    });
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
