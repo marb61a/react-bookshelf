04c3741620eb06e73eb4804001c9bd5b5204697d
martin byrne <marb61a@gmail.com>
2018-06-07 20:27:33 +0000

    added lastname and email elements and handlers to admin register container

--- client/src/containers/Admin/register.js
+++ client/src/containers/Admin/register.js
@@ -22,19 +22,59 @@ class Register extends PureComponent {
         });
     }
     
+    handleInputLastname = (event) => {
+        this.setState({
+            lastname: event.target.value
+        });
+    }
+    
+    handleInputEmail = (event) => {
+        this.setState({
+            email:event.target.value
+        });
+    }
+    
+    submitForm = (e) => {
+        e.preventDefault();
+        this.setState({error:''});
+        
+        this.props.dispatch(userRegister({
+            email: this.state.email,
+            password: this.state.password,
+            name: this.state.name,
+            lastname: this.state.lastname
+        }, this.props.users.user));
+    }
+    
     render(){
         let user = this.props.user;
         
         return (
             <div className="rl_container">
-                <form>
+                <form onSubmit={this.submitForm}>
                     <h2>Add user</h2>
                     <div className="form_element">
                         <input 
                             type="text"
-                            placeholder="Enter Name"
+                            placeholder="Enter First Name"
                             value={this.state.name}
                             onChange={this.handleImputname}
+                        />
+                    </div>
+                    <div className="form_element">
+                        <input
+                            type="text"
+                            placeholder="Enter Lastname"
+                            value={this.state.lastname}
+                            onChange={this.handleInputLastname}
+                         />
+                    </div>
+                    <div className="form_element">
+                        <input 
+                            type="email"
+                            placeholder="Enter Email Address"
+                            value={this.state.email}
+                            onChange={this.handleInput}
                         />
                     </div>
                 </form>
