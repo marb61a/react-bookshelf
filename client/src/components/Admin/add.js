11f19f51ab7a38f494ea1f62104410f4f5449170
martin byrne <marb61a@gmail.com>
2018-06-11 15:42:13 +0000

    added form and inout methods to admin add file

--- client/src/components/Admin/add.js
+++ client/src/components/Admin/add.js
@@ -15,11 +15,50 @@ class AddBook extends Component {
         }
     }
     
+    handleInput = (event, name) => {
+        const newFormdata = {
+            ...this.state.formdata
+        };
+        newFormdata[name] = event.target.value
+        
+        this.setState({
+            formdata: newFormdata    
+        });
+    }
+    
+    submitForm = (e) => {
+        e.preventDefault();
+        this.props.dispatchk(addBook({
+            ...this.state.formdata,
+            ownerId: this.props.user.login.id
+        }));
+    }
+    
     render(){
         return (
             <div className="rl_container article">
-                <form>
-                
+                <form onSubmit={this.submitForm}>
+                    <h2>Add a review</h2>
+                    <div className="form_element">
+                        <input 
+                            type="text"
+                            placeholder="Enter Name"
+                            value={this.state.formdata.name}
+                            onChange={(event) => this.handleInput(event, 'name')}
+                        />
+                    </div>
+                    <div className="form_element">
+                        <input
+                            type="text"
+                            placeholder="Enter author"
+                            value={this.state.formdata.author}
+                            onChange={(event)=>this.handleInput(event,'author')}
+                        />
+                    </div>
+                    <textarea 
+                        value={this.formdata.value.review}
+                        onChange={(event)=>this.handleInput(event,'review')}
+                    />
                 </form>
             </div>    
         );
