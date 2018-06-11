aabfd195ee0614ba784002ce821cfecb736f8a62
martin byrne <marb61a@gmail.com>
2018-06-11 20:05:27 +0000

    added get book and clear book with reviewer actions and reducers

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -17,6 +17,39 @@ export function getBooks(limit = 10, start = 0, order = 'asc', list = ''){
         };
 }
 
+export function getBookWithReviewer(id){
+    const request = axios.get(`/api/getBook?id=${id}`);
+    
+    return (dispatch) =>{
+        request.then(({data}) => {
+            let book = data;
+                    
+            axios.get(`/api/getReviewer?id=${book.ownerId}`)
+                .then(({data}) => {
+                    let response = {
+                        book,
+                        reviewer: data
+                    };
+                    
+                    dispatch({
+                        type:'GET_BOOK_W_REVIEWER',
+                        payload:response
+                    });
+                });
+        });  
+    };
+}
+
+export function clearBookWithReviewer(){
+    return {
+        type: 'CLEAR_BOOK_W_REVIEWER',
+        payload: {
+            book: {},
+            reviewer: {}
+        }
+    };
+}
+
 export function addBook(book){
     const request = axios.post('/api/book',book)
         .then(response => response.data);
--- client/src/reducers/books_reducer.js
+++ client/src/reducers/books_reducer.js
@@ -10,6 +10,18 @@ export default function(state = {}, action){
                 ...state,
                 book: action.payload
             };
+        case 'GET_BOOK_W_REVIEWER':
+            return {
+                ...state,
+                book: action.payload.book,
+                reviewer: action.payload.reviewer
+            };
+        case 'CLEAR_BOOK_W_REVIEWER':
+            return{
+                ...state,
+                book: action.payload.book,
+                reviewer: action.payload.reviewer
+            };
         case 'CLEAR_BOOK':
             return {
                 ...state,
